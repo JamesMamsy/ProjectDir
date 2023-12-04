e256ee046bc1eceff2a8c9397095164a77b3a86a
NotKobe <al.10549329@gmail.com>
2023-12-04 15:22:16 -0600

    got original alberto kmeans back

--- app/pages/K-means.py
+++ app/pages/K-means.py
@@ -1,27 +1,47 @@
 import streamlit as st
-st.markdown("# K-Means")
-st.sidebar.header("K-means")
+import pandas as pd
+from models.kmeans import KMeans
 
-#Carrier Delay
-carrier_delay = st.sidebar.number_input("Carrier Delay (Minutes)", value = None, placeholder = "Enter a number")
+# Function to load and preprocess data for clustering
+def load_data():
+    # load the df, CHECK PATH HERE 
+    flights_df = pd.read_csv(r"data\flights.csv")
 
-#Arrival Delay
-dept_delay = st.sidebar.number_input("Arrival Delay (Minutes)", value = None, placeholder="Enter a number")
+    # select columns to do clustering on and drop missing values
+    flights_df = flights_df[["DEP_DELAY", "ARR_DELAY"]].dropna()
 
-#Submit 
-cluster = st.sidebar.button("Estimate Total Delay")
-
-if(cluster and (carrier_delay == None or dept_delay == None) ):
-    st.sidebar.write("Please enter in a value")
-elif(cluster):
-
-    #avg_delay = ???
-    #
-    st.markdown("## Estimated Total Delay:")
-    st.markdown("(Average Cluster Delay )")
-    st.markdown("### 35 Minutes")
-    st.markdown("## Cluster: ")
-    st.markdown("### Carrier:")
-    st.markdown("### Arrival:")
+    return flights_df
 
 
+def main():
+    st.title("Airline delay app")
+
+    df = load_data()
+
+    # Get user input for DEP_DELAY and ARR_DELAY
+    dep_delay = st.number_input("Enter DEP_DELAY:", min_value=0.0)
+    arr_delay = st.number_input("Enter ARR_DELAY:", min_value=0.0)
+
+    kmeans = KMeans(k=4)
+
+    # Check if the user has pressed the "PREDICT CLUSTER" button
+    if st.button("PREDICT CLUSTER"):
+        kmeans.fit(df)
+
+        # call predict function from kmeans class to see closest cluster of input points
+        predicted_cluster, closest_centroid_distance = kmeans.predict(dep_delay, arr_delay, df)
+
+        st.success(f"Predicted Cluster: {predicted_cluster}")
+        st.info(f"Distance to Closest Centroid: {round(closest_centroid_distance,3)}")
+
+    # Check if the user has pressed the "SHOW CLUSTERS" button
+    if kmeans is not None and st.button("SHOW CLUSTERS INFO"):
+        kmeans.fit(df)
+        # Show cluster information without fitting again
+        cluster_info_output = kmeans.cluster_info(df)
+        for info_line in cluster_info_output:
+            st.write(info_line)
+
+
+if __name__ == "__main__":
+    main()
