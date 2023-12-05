c9503f910301173acb3d0a95ccde05d603006f04
Elizabeth B <welizabethcarole@gmail.com>
2023-12-04 18:31:15 -0600

    init py update

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,3 @@
-#from models.apriori import FlightDelayApriori
+from models.apriori import FlightDelayApriori
 from models.kmeans import KMeans
-from models.logisticRegression import LogisiticRegression
+from models.logisiticRegression import LogisiticRegression
